1b58bc44b2e563134578651f4430d24687d68cff
steveclifton <steve.clifton@outlook.com>
2016-11-15 22:24:12 +1300

    Set the leftside panel view

--- Views/layouts/leftside.php
+++ Views/layouts/leftside.php
@@ -1,6 +1,15 @@
-<ul class="nav">
-    <li><a href="#">Home</a></li>
-    <li><a href="#">Link 1</a></li>
-    <li><a href="#">Link 2</a></li>
-    <li><a href="#">Link 3</a></li>
-</ul>
+<div class="col-xs-6 col-sm-3 sidebar-offcanvas" id="sidebar" role="navigation">
+    <ul class="nav">
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Link 1</a></li>
+        <li><a href="#">Link 2</a></li>
+        <li><a href="#">Link 3</a></li>
+<!--        --><?php
+//        echo "<li><a>Weeks</a></li>";
+//        $weeks = ['1', '2', '3', '4', '5'];
+//        foreach ($weeks as $week) {
+//            print "<li><a href=\"#\">Week $week</a></li>";
+//        }
+//        ?>
+    </ul>
+</div>
