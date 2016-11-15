87688418c69b4784b8fc8cd8e8efb774220d24f7
steveclifton <steve.clifton@outlook.com>
2016-11-15 21:30:29 +1300

    Implement sidebar

---
+++ Views/layouts/leftside.php
@@ -0,0 +1,6 @@
+<ul class="nav">
+    <li class="active"><a href="#">Home</a></li>
+    <li><a href="#">Link 1</a></li>
+    <li><a href="#">Link 2</a></li>
+    <li><a href="#">Link 3</a></li>
+</ul>
