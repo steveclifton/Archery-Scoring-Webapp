71bac3727f46698f116707d8612aa1a7d3ceb544
steveclifton <steve.clifton@outlook.com>
2016-11-16 21:19:40 +1300

    Remove title

--- Views/welcome.view.php
+++ Views/welcome.view.php
@@ -1,5 +1,4 @@
-    <h1 style="padding-bottom: 10px; font-family: 'Droid Sans', sans-serif;">Archery NZ - Outdoor League Series</h1>
-    <br>
+<br>
     <p>
         Here are some scores, some info, some other stuff.       Here are some scores, some info, some other stuff.
         Here are some scores, some info, some other stuff.       Here are some scores, some info, some other stuff.
