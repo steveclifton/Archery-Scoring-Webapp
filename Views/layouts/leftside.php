00ea3b856422ee3169f577626da16358c700d058
steveclifton <steve.clifton@outlook.com>
2016-11-16 21:20:19 +1300

    Update view for button

--- Views/layouts/leftside.php
+++ Views/layouts/leftside.php
@@ -1,15 +1,20 @@
-<div class="col-xs-6 col-sm-3 sidebar-offcanvas" id="sidebar" role="navigation">
-    <ul class="nav">
-        <li><a href="#">Home</a></li>
-        <li><a href="#">Link 1</a></li>
-        <li><a href="#">Link 2</a></li>
-        <li><a href="#">Link 3</a></li>
-<!--        --><?php
-//        echo "<li><a>Weeks</a></li>";
-//        $weeks = ['1', '2', '3', '4', '5'];
-//        foreach ($weeks as $week) {
-//            print "<li><a href=\"#\">Week $week</a></li>";
-//        }
-//        ?>
+<div class="col-xs-6 col-sm-2 sidebar-offcanvas" id="sidebar" role="navigation" style="padding-top: 10px">
+
+    <ul class="nav" style="padding-top: 14px">
+        <?php
+            if (isset($_SESSION['id'])) {
+                echo "<a class=\"btn-success btn pull-left\">Submit Score!</a><br><br><br>";
+            } else {
+                echo "<br><br><br>";
+            }
+
+
+            $weeks = ['1', '2', '3', '4', '5'];
+
+            foreach ($weeks as $week) {
+                print "<li><a href=\"#\">Week $week</a></li>";
+            }
+        ?>
     </ul>
+
 </div>
