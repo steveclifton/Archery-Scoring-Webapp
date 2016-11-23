3e8ed10dcbd75935aa7d17be0422280435842037
steveclifton <steve.clifton@outlook.com>
2016-11-24 08:07:53 +1300

    Makes the footer container-fluid

--- Views/layouts/footer.php
+++ Views/layouts/footer.php
@@ -1,7 +1,7 @@
 <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro" rel="stylesheet">
 
 <div class="navbar navbar-default navbar-fixed-bottom">
-    <div class="container">
+    <div class="container-fluid">
         <div class="btn-toolbar">
             <a class="navbar-btn btn-primary btn pull-left" href="http://www.facebook.com/ArcheryNZIndoor" target="_blank">
                 Facebook!
