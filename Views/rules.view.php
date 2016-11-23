70b5048c6cd2605ab5e9c310dcf8fb24db112039
steveclifton <steve.clifton@outlook.com>
2016-11-24 08:08:20 +1300

    Add rules view

---
+++ Views/rules.view.php
@@ -0,0 +1,71 @@
+<h3>ANZ Outdoor League</h3>
+<br>
+<p>
+    The ANZ League indoor league series has been a great success over this winter season. We have had
+    over 90 individual archers join in on submitting scores, with several archers have chosen to shoot
+    multiple disciplines.
+    Multiple clubs and individuals have expressed great interest in continuing this league series past the
+    indoor season and to create an outdoor equivalent, so we have decided to continue this shoot over
+    the summer months (with some changes to the round being shot).
+</p>
+<p>
+    <ul>
+        <li>
+            The event format is a 30m ‘360’ round – 36 arrows at 30m, 80cm target. Archers may opt to
+            use either a reduced face (6-10) or a single full sized face.
+        </li>
+        <li>
+            Events shall be run to World Archery rules, but without a requirement for timing or judges.
+        </li>
+        <li>
+            Divisions shall be: Open Compound, Open Recurve, Open Recurve Barebow, Open Longbow,
+            Open Crossbow (no gender or age divisions)
+        </li>
+        <li>
+            Participants must be members of ArcheryNZ.
+        </li>
+        <li>
+            There shall be one round per calendar week (Monday-Sunday) from November 7th until 12th
+            March (break from 26-Dec – 15-Jan & total of 15 weeks over 2016-2017).
+        </li>
+        <li>
+            Clubs may organise multiple timeslots to shoot the event, but competitors may only shoot
+            one round each per week. Competitors must nominate that they are shooting a League
+            round before commencing, and the round shall be valid for that week as soon as the first
+            scoring end is commenced.
+        </li>
+        <li>
+            All scores must be verified by at least one member of each participating club, and emailed to
+            the ANZ Indoor League Coordinator no later than 6pm the following Monday.
+        </li>
+        <li>
+            Entrance to the League is free of charge for all ArcheryNZ members.
+        </li>
+        <li>
+            Within each division, there shall be an overall championship and a handicapped
+            championship:
+        <ul>
+            <li>
+                The Overall Championship shall be determined based on the highest aggregate score
+                of each archer’s top 10 submitted scores.
+            </li>
+        </ul>
+        <li>
+            The Handicapped Championship shall be determined based on the highest number
+            of Points, awarded as follows:
+        </li>
+        <ul>
+            <li>
+                i. An archer’s handicap for each week is calculated = (360 – mean average of
+                previous rounds shot to date in the calendar year). This handicap is added
+                to the archers score to give an adjusted total.
+            </li>
+            <li>
+                ii. Points are awarded based on the adjusted totals as follows. There shall be
+                no tie breaks – archers finishing on the same score shall be declared of equal
+                position.
+            </li>
+        </ul>
+    </ul>
+</p>
+<br><br><br>
