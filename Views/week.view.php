76b9cab648f1003196fe78ae2cf818334d85974a
Steve <stephen.clifton.1@uni.massey.ac.nz>
2016-12-01 11:03:35 +1300

    Add form to week view

---
+++ Views/week.view.php
@@ -0,0 +1,24 @@
+<?php
+if (isset($viewData[0])) {
+    print_r($viewData);
+}
+?>
+<div class="form-group">
+    <form action="/week" method="post" name="scoreform" onsubmit="return validateForm()">
+        <h3 style="margin-left: 14px">Submit <?= $_SESSION['first_name'] ?>'s score here!</h3>
+        <div class="col-md-2">
+            <input type="text" id="score" class="form-control" name="score" placeholder="Score" required>
+        </div>
+        <div class="col-md-2">
+            <input type="text" id="xcount" class="form-control" name="xcount" placeholder="X-Count" required>
+        </div>
+        <div class="col-md-2">
+            <input id="submit" type="submit" name="submit" class="btn btn-danger" value="Submit">
+        </div>
+        <input type="hidden" name="week" value="<?= $_GET['num'] ?>">
+        <br>
+    </form>
+</div>
+<div id="row">
+    <p id="incorrect" style="color: red"></p>
+</div>
