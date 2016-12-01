330901ff9fd685c7966537bb4e4d110124d81905
Steve <stephen.clifton.1@uni.massey.ac.nz>
2016-12-01 15:21:42 +1300

    Refactor view for results

--- Views/week.view.php
+++ Views/week.view.php
@@ -4,23 +4,37 @@ if (isset($_SESSION['id'])) {
         include('Views/layouts/submitscore.php');
     }
 }
+
 ?>
-<div class="form-group">
-    <form action="/week" method="post" name="scoreform" onsubmit="return validateForm()">
-        <h3 style="margin-left: 14px">Submit <?= $_SESSION['first_name'] ?>'s score here!</h3>
-        <div class="col-md-2">
-            <input type="text" id="score" class="form-control" name="score" placeholder="Score" required>
-        </div>
-        <div class="col-md-2">
-            <input type="text" id="xcount" class="form-control" name="xcount" placeholder="X-Count" required>
-        </div>
-        <div class="col-md-2">
-            <input id="submit" type="submit" name="submit" class="btn btn-danger" value="Submit">
-        </div>
-        <input type="hidden" name="week" value="<?= $_GET['num'] ?>">
-        <br>
-    </form>
-</div>
-<div id="row">
-    <p id="incorrect" style="color: red"></p>
+
+
+<div class="table-responsive">
+    <div class="container">
+        <h2>Results</h2>
+        <p>Current weeks results</p>
+        <table class="table table-condensed">
+            <thead>
+            <tr>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Score</th>
+                <th>X-Count</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <td>Mary</td>
+                <td>Moe</td>
+                <td>359</td>
+                <td>31</td>
+            </tr>
+            <tr>
+                <td>July</td>
+                <td>Dooley</td>
+                <td>358</td>
+                <td>31</td>
+            </tr>
+            </tbody>
+        </table>
+    </div>
 </div>
