d61da9fce520a4d0e2783ef4c14c6a9c03303858
Steve <stephen.clifton.1@uni.massey.ac.nz>
2016-12-01 15:22:19 +1300

    Removed submit button, moved to week view

--- Views/layouts/leftside.php
+++ Views/layouts/leftside.php
@@ -2,14 +2,7 @@
 
     <ul class="nav" style="padding-top: 14px">
         <?php
-            if (isset($_SESSION['id'])) {
-                echo "<a class=\"btn-success btn pull-left\" href='/submitscore' style=\"margin-left: 20px\">Submit Score!</a><br><br><br>";
-            } else {
-                echo "<br><br><br>";
-            }
-
-
-            $weeks = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '9', '10', '11', '12', '13', '14', '15'];
+            $weeks = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15'];
 
             foreach ($weeks as $week) {
                 echo "<li><a href=\"/week?week=$week\" style='text-decoration: underline; font-size: 12pt;'>Week $week</a></li>";
