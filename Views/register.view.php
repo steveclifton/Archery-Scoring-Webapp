126d13d554ace65e82d7444061f82787d283b5a8
Steve <stephen.clifton.1@uni.massey.ac.nz>
2016-12-05 22:40:25 +1300

    Add form for Registration

--- Views/register.view.php
+++ Views/register.view.php
@@ -1,23 +1,62 @@
-<div id="center">
-    <p class="profile">Create New Profile</p>
-    <form action="register" method="post" onsubmit="return check_data(this)">
-        <input type="text" name="first_name" placeholder="First Name" required>
+<form class="form-horizontal" action="/register" method="POST">
+    <fieldset>
+        <div id="legend">
+            <legend class="">Request A New Account</legend>
+            <p>Accounts are usually approved within 24-Hours</p>
+        </div>
+        <div class="control-group">
+            <label class="control-label" for="first_name">First Name</label>
+            <div class="controls">
+                <input type="text" id="first_name" name="first_name" placeholder="" class="input-xlarge" required>
+            </div>
+        </div>
+        <div class="control-group">
+            <label class="control-label" for="last_name">Last Name</label>
+            <div class="controls">
+                <input type="text" id="last_name" name="last_name" placeholder="" class="input-xlarge" required>
+            </div>
+        </div>
+        <div class="control-group">
+            <label class="control-label" for="password">Email</label>
+            <div class="controls">
+                <input type="email" id="email" name="email" placeholder="" class="input-xlarge" required>
+            </div>
+        </div>
+        <div class="control-group">
+            <!-- Password-->
+            <label class="control-label" for="password">Password</label>
+            <div class="controls">
+                <input type="password" id="password" name="password" placeholder="" class="input-xlarge" required>
+            </div>
+        </div>
+        <div class="control-group">
+            <!-- Password-->
+            <label class="control-label" for="password">Confirm Password</label>
+            <div class="controls">
+                <input type="password" id="confirm_password" name="confirm_password" placeholder="" class="input-xlarge" required>
+            </div>
+        </div>
+        <div class="control-group">
+            <label class="control-label"  for="student_id">ANZ Number</label>
+            <div class="controls">
+                <input type="text" id="anz_num" name="anz_num" class="input-xlarge" required>
+            </div>
+        </div>
+        <div class="control-group">
+            <!-- Club-->
+            <label class="control-label" for="password">Club</label>
+            <div class="controls">
+                <input type="text" id="club" name="club" class="input-xlarge" required>
+            </div>
+        </div>
         <br>
-        <input type="text" name="last_name" placeholder="Last Name" required>
-        <br>
-        <input type="email" name="email" id="emailfield" placeholder="Email" required>
-        <br>
-        <span id="email_errormessage"></span>
-        <br>
-        <input type="text" name="username" id="usernamefield" placeholder="Username" required>
-        <br>
-        <span id="username_errormessage"></span>
-        <br>
-        <input type="password" name="password" placeholder="Password" required>
-        <br>
-        <input type="password" name="password2" placeholder="Confirm Password" required>
-        <br><br>
-        <input name="submit" type="submit" value="Submit">
-    </form>
-</div>
+        <div class="control-group">
+            <div class="controls">
+                <button class="btn btn-success" id="register">Register</button>
+            </div>
+        </div>
 
+            }
+        ?>
+    </fieldset>
+</form>
