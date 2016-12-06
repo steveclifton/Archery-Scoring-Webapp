282d4fb453ee35ca42a68e96147ad2b3bf7ae541
Steve <stephen.clifton.1@uni.massey.ac.nz>
2016-12-06 16:03:58 +1300

    Class allows for config info to be in 1 place

--- Configurations/Event.Config.php
+++ Configurations/Event.Config.php
@@ -1,6 +1,20 @@
 <?php
 
+namespace Archery\Configurations;
+
+
 class Event
 {
+    private $currentEvent = 'Outdoor League Series';
+    private $dbTableName = '30m_round';
 
+    public function getCurrentEventName()
+    {
+        return $this->currentEvent;
+    }
+
+    public function getTableName()
+    {
+        return $this->dbTableName;
+    }
 }
