dce1f1879ef30764e122eabb504e394c2972bbd5
Steve <stephen.clifton.1@uni.massey.ac.nz>
2016-12-08 12:27:54 +1300

    Add views for other divisons

--- Views/week.view.php
+++ Views/week.view.php
@@ -67,5 +67,61 @@ if (isset($_SESSION['id'])) {
             <?php } ?>
             </tbody>
         </table>
+        </table>
+        <h3 style="text-align: center;">Recurve Barebow</h3>
+        <table class="table table-bordered table-hover">
+            <thead>
+            <tr>
+                <th>#</th>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Score</th>
+                <th>X-Count</th>
+            </tr>
+            </thead>
+            <tbody>
+            <?php
+            $i = 1;
+            foreach ($viewData as $data) { ?>
+                <?php if ($data['division'] == 'recurve-bb') {?>
+                    <tr>
+                        <th scope="row"> <?php echo $i; $i++; ?></th>
+                        <td> <?= $data['first_name'] ?></td>
+                        <td> <?=$data['last_name'] ?></td>
+                        <td> <?=$data['score'] ?></td>
+                        <td> <?=$data['xcount'] ?></td>
+                    </tr>
+                <?php } ?>
+            <?php } ?>
+            </tbody>
+        </table>
+        </table>
+        <h3 style="text-align: center;">Longbow</h3>
+        <table class="table table-bordered table-hover">
+            <thead>
+            <tr>
+                <th>#</th>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Score</th>
+                <th>X-Count</th>
+            </tr>
+            </thead>
+            <tbody>
+            <?php
+            $i = 1;
+            foreach ($viewData as $data) { ?>
+                <?php if ($data['division'] == 'longbow') {?>
+                    <tr>
+                        <th scope="row"> <?php echo $i; $i++; ?></th>
+                        <td> <?= $data['first_name'] ?></td>
+                        <td> <?=$data['last_name'] ?></td>
+                        <td> <?=$data['score'] ?></td>
+                        <td> <?=$data['xcount'] ?></td>
+                    </tr>
+                <?php } ?>
+            <?php } ?>
+            </tbody>
+        </table>
     </div>
 </div>
