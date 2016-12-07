e81c9a5e087eec0ac0f86b00a675ea6f0696c1ad
Steve <stephen.clifton.1@uni.massey.ac.nz>
2016-12-07 15:36:51 +1300

    Using basic html select for testing

--- Views/layouts/submitscore.php
+++ Views/layouts/submitscore.php
@@ -1,13 +1,3 @@
-
-<!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.12.1/css/bootstrap-select.min.css">
-
-<!-- Latest compiled and minified JavaScript -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.12.1/js/bootstrap-select.min.js"></script>
-
-<!-- (Optional) Latest compiled and minified JavaScript translation files -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.12.1/js/i18n/defaults-*.min.js"></script>
-
 <div class="form-group" style="margin-left: 22px">
     <form action="/submitscore" method="POST" name="scoreform" onsubmit="return validateForm()">
         <h3 style="margin-left: 14px">Submit <?= $_SESSION['first_name'] ?>'s score here!</h3>
@@ -18,18 +8,29 @@
             <input type="text" id="xcount" class="form-control" name="xcount" placeholder="X-Count" required>
         </div>
         <div class="col-md-3">
-            <select name="division" id="division" class="selectpicker show-menu-arrow">
-                <option value="compound">Compound</option>
-                <option value="recurve">Recurve</option>
-                <option value="recurve_bb">Recurve Barebow</option>
-                <option value="longbow">Longbow</option>
-                <option value="crossbow">Crossbow</option>
+<!--            <select name="division" id="bow_division" class="selectpicker show-menu-arrow">-->
+<!--                <option value="compound" id="Compound">Compound</option>-->
+<!--                <option value="recurve" id="Recurve">Recurve</option>-->
+<!--                <option value="recurve_bb" id="Recurve_bb">Recurve Barebow</option>-->
+<!--                <option value="longbow" id="Longbow">Longbow</option>-->
+<!--                <option value="crossbow" id="Crossbow">Crossbow</option>-->
+<!--            </select>-->
+            <div class="bow">
+            <select name="division" id="division">
+                <option value="compound" id="Compound">Compound</option>
+                <option value="recurve" id="Recurve">Recurve</option>
+                <option value="recurve_bb" id="Recurve_bb">Recurve Barebow</option>
+                <option value="longbow" id="Longbow">Longbow</option>
+                <option value="crossbow" id="Crossbow">Crossbow</option>
             </select>
+            </div>
         </div>
+
         <div class="col-md-2">
             <input id="submit" type="submit" name="submit" class="btn btn-success" value="Submit">
         </div>
         <input type="hidden" name="week" value="<?= $_GET['week'] ?>">
+        <input type="hidden" id="prefered_type" value="<?= $_SESSION['prefered_type'] ?>">
         <br>
     </form>
 </div>
