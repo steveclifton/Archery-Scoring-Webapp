7fe4c2819c2b61bd2437a0dbdb4e3b5cda2e78b5
Steve <stephen.clifton.1@uni.massey.ac.nz>
2016-12-16 11:50:51 +1300

    Add bowtype to form

--- Views/profile.view.php
+++ Views/profile.view.php
@@ -30,13 +30,7 @@
         <div class="control-group">
             <label class="control-label">Club</label>
             <div class="controls">
-                <input type="text" id="club" name="club" class="input-xlarge" value="<?= $viewData['club'] ?>" required>
-            </div>
-        </div>
-        <div class="control-group">
-            <label class="control-label">Date of Birth (dd/mm/yyyy)</label>
-            <div class="controls">
-                <input type="text" id="dob" name="dob" class="input-xlarge" value="<?= (isset($viewData['dob']) ? $viewData['dob'] : ""); ?>" required>
+                <input type="text" id="club" name="club" class="input-xlarge" value="<?= $viewData['user']['club'] ?>" required>
             </div>
         </div>
         <div class="control-group">
@@ -52,28 +46,23 @@
             </div>
         </div>
         <div class="control-group">
-            <label class="control-label">Gender</label><br>
-            <select name="gender" id="gender" class="selectpicker show-menu-arrow">
-                <option value=""></option>
-                <option value="male">Male</option>
-                <option value="female">Female</option>
-            </select>
-        </div>
-        <div class="control-group">
             <!-- Prefered Bow Type-->
             <label class="control-label">Prefered Bow Type</label><br>
-            <select name="prefered_type" id="prefered_type" class="selectpicker show-menu-arrow">
-                <option value="compound">Compound</option>
-                <option value="recurve">Recurve</option>
-                <option value="recurve_bb">Recurve Barebow</option>
-                <option value="longbow">Longbow</option>
-                <option value="crossbow">Crossbow</option>
-            </select>
+                <div class="bow">
+                    <select name="prefered_type" id="division">
+                        <option value="compound" id="Compound">Compound</option>
+                        <option value="recurve" id="Recurve">Recurve</option>
+                        <option value="recurve_bb" id="Recurve_bb">Recurve Barebow</option>
+                        <option value="longbow" id="Longbow">Longbow</option>
+                        <option value="crossbow" id="Crossbow">Crossbow</option>
+                    </select>
+                </div>
+            <input type="hidden" id="prefered_type" value="<?= $_SESSION['prefered_type'] ?>">
         </div>
-        <br><br>
+        <br>
         <div class="control-group">
             <div class="controls">
-                <button class="btn btn-success" id="register">Register</button>
+                <button class="btn btn-success" id="update">Update</button>
             </div>
         </div>
         <?php
@@ -83,7 +72,6 @@
             } else {
                 echo "<p style='color: red'>Account Creation Failed, please check details and try again</p>";
             }
-
         }
         ?>
     </fieldset>
