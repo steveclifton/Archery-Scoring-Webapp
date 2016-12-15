2fce377a52b6cd8c320b9904f67b2151419513a6
Steve <stephen.clifton.1@uni.massey.ac.nz>
2016-12-15 22:13:35 +1300

    Refactor code

--- Views/register.view.php
+++ Views/register.view.php
@@ -5,53 +5,53 @@
             <p>Accounts are usually approved within 24-Hours</p>
         </div>
         <div class="control-group">
-            <label class="control-label" for="first_name">First Name</label>
+            <label class="control-label">First Name</label>
             <div class="controls">
                 <input type="text" id="first_name" name="first_name" placeholder="" class="input-xlarge" required>
             </div>
         </div>
         <div class="control-group">
-            <label class="control-label" for="last_name">Last Name</label>
+            <label class="control-label">Last Name</label>
             <div class="controls">
                 <input type="text" id="last_name" name="last_name" placeholder="" class="input-xlarge" required>
             </div>
         </div>
         <div class="control-group">
-            <label class="control-label" for="password">Email</label>
+            <label class="control-label">Email</label>
             <div class="controls">
                 <input type="email" id="email" name="email" placeholder="" class="input-xlarge" required>
             </div>
         </div>
         <div class="control-group">
             <!-- Password-->
-            <label class="control-label" for="password">Password</label>
+            <label class="control-label">Password</label>
             <div class="controls">
                 <input type="password" id="password" name="password" placeholder="" class="input-xlarge" required>
             </div>
         </div>
         <div class="control-group">
             <!-- Password-->
-            <label class="control-label" for="password">Confirm Password</label>
+            <label class="control-label">Confirm Password</label>
             <div class="controls">
                 <input type="password" id="confirm_password" name="confirm_password" placeholder="" class="input-xlarge" required>
             </div>
         </div>
         <div class="control-group">
-            <label class="control-label"  for="student_id">ANZ Number</label>
+            <label class="control-label">ANZ Number</label>
             <div class="controls">
                 <input type="text" id="anz_num" name="anz_num" class="input-xlarge" required>
             </div>
         </div>
         <div class="control-group">
             <!-- Club-->
-            <label class="control-label" for="password">Club</label>
+            <label class="control-label">Club</label>
             <div class="controls">
                 <input type="text" id="club" name="club" class="input-xlarge" required>
             </div>
         </div>
         <div class="control-group">
             <!-- Prefered Bow Type-->
-            <label class="control-label" for="password">Prefered Bow Type</label><br>
+            <label class="control-label">Prefered Bow Type</label><br>
             <select name="prefered_type" id="prefered_type" class="selectpicker show-menu-arrow">
                 <option value="compound">Compound</option>
                 <option value="recurve">Recurve</option>
