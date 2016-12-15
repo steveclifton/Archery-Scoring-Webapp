88174ed95c4b116408af09de4047155b90fbf398
Steve <stephen.clifton.1@uni.massey.ac.nz>
2016-12-15 16:20:34 +1300

    Refactor display

--- Views/admin.view.php
+++ Views/admin.view.php
@@ -1,33 +1,37 @@
-<div class="table-responsive">
+<h3>Pending Users : <?= count($viewData['pending_users'])?></h3>
+<input type="button" value="Show" id="pendingbutton" class="btn btn-danger">
+<hr>
+
+<div class="table-responsive" id="pendingusers">
     <div class="container">
-        <h3>Pending Users</h3>
-            <table class="table table-bordered table-hover">
-                <thead>
-                <tr>
-                    <th>First Name</th>
-                    <th>Last Name</th>
-                    <th>Anz Num</th>
-                    <th>Email</th>
-                    <th>Club</th>
-                    <th>Authorise</th>
-                </tr>
-                </thead>
-                <tbody>
-                <?php
+        <table class="table table-bordered table-hover">
+            <thead>
+            <tr>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Anz Num</th>
+                <th>Email</th>
+                <th>Club</th>
+                <th>Authorise</th>
+            </tr>
+            </thead>
+            <tbody>
+            <?php
                 foreach ($viewData['pending_users'] as $user) { ?>
                     <tr>
                         <form action="/confirmusers" method="POST" name="usersconfirm">
-                            <td><input type="text" value="<?=$user['first_name']?>" name="first_name"></td>
-                            <td><input type="text" value="<?=$user['last_name']?>" name="last_name"></td>
-                            <td><input type="text" value="<?=$user['anz_num']?>" name="anz_num"></td>
-                            <td><input type="text" value="<?=$user['email']?>" name="email"></td>
-                            <td><input type="text" value="<?=$user['club']?>" name="club"></td>
-                            <td><input id="submit" type="submit" name="submit" class="btn btn-success" value="Authorise"></td>
+                            <td><input type="text" value="<?= $user['first_name'] ?>" name="first_name"></td>
+                            <td><input type="text" value="<?= $user['last_name'] ?>" name="last_name"></td>
+                            <td><input type="text" value="<?= $user['anz_num'] ?>" name="anz_num"></td>
+                            <td><input type="text" value="<?= $user['email'] ?>" name="email"></td>
+                            <td><input type="text" value="<?= $user['club'] ?>" name="club"></td>
+                            <td><input id="submit" type="submit" name="submit" class="btn btn-success" value="Authorise">
+                            </td>
                         </form>
                     </tr>
                 <?php } ?>
-                </tbody>
-            </table>
+            </tbody>
+        </table>
     </div>
 </div>
 
