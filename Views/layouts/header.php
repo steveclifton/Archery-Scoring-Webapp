263ea7106bbc54452d9e33452b35e86af7ca1e38
Steve <stephen.clifton.1@uni.massey.ac.nz>
2016-12-15 22:13:56 +1300

    Add routing

--- Views/layouts/header.php
+++ Views/layouts/header.php
@@ -43,8 +43,8 @@
                                 <li class='dropdown'>
                                     <a href=\"#\" class=\"dropdown-toggle\" data-toggle=\"dropdown\">$username <b class=\"caret\"></b></a>
                                     <ul class=\"dropdown-menu\">
-                                        <li><a href='#'>Profile</a></li>
-                                        <li><a href='logout'>Logout</a></li>
+                                        <li><a href='/userprofile'>Profile</a></li>
+                                        <li><a href='/logout'>Logout</a></li>
                                     </ul>
                                 </li>
                         ";
