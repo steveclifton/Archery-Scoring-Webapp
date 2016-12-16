27afdb593c54cf8ff4df228bc956eb4be320f221
Steve <stephen.clifton.1@uni.massey.ac.nz>
2016-12-16 18:15:00 +1300

    Add footer

--- Views/layouts/footer.php
+++ Views/layouts/footer.php
@@ -1,14 +1,3 @@
-<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro" rel="stylesheet">
-
-<div class="navbar navbar-default navbar-fixed-bottom">
-    <div class="container-fluid">
-        <div class="btn-toolbar">
-            <a class="navbar-btn btn-primary btn pull-left" href="http://www.facebook.com/ArcheryNZIndoor" target="_blank">
-                Facebook!
-            </a>
-            <a class="navbar-btn btn-danger btn pull-left" href="http://www.archerynz.co.nz" target="_blank">
-                Archery NZ
-            </a>
-        </div>
-    </div>
+<div class="navbar">
+    <br><br><br><br><br><br><br><br><br>
 </div>
