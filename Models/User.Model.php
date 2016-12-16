990cb9ee10b12631a59b162ef9d51db4d3ce1bec
Steve <stephen.clifton.1@uni.massey.ac.nz>
2016-12-16 18:27:02 +1300

    Add return of current status to query

--- Models/User.Model.php
+++ Models/User.Model.php
@@ -150,9 +150,12 @@ class User extends Base
         return $success;
     }
 
+    /**
+     * Returns all users that the logged in user has an association with
+     */
     public function getAssociatedUsers($userId)
     {
-        $sql = "SELECT users.first_name, users.last_name, users.anz_num, users.club, users.email 
+        $sql = "SELECT users.first_name, users.last_name, users.anz_num, users.club, users.email, join_users.status 
                 FROM `join_users` 
                 INNER JOIN users 
                 ON `join_users`.associate_id = users.id 
