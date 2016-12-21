6e21c2022fcbf3fe73ab1687a4548412bfc77c50
Steve <stephen.clifton.1@uni.massey.ac.nz>
2016-12-22 09:04:03 +1300

    Update score form to new system

--- Views/week.view.php
+++ Views/week.view.php
@@ -1,13 +1,49 @@
 <h2 style="text-align: center;">Week <?= $_GET['week']; ?> Results</h2>
-<hr>
 
 <?php
-if (isset($_SESSION['id'])) {
-    include('Views/layouts/submitscore.php');
-    echo "<hr>";
-}
+//if (isset($_SESSION['id'])) {
+//    include('Views/layouts/submitscore.php');
+//    echo "<hr>";
+//}
+//
+//?>
 
-?>
+<div id="legend">
+    <legend>Submit Scores</legend>
+</div>
+<form class="form-inline" action="/submitscore" method="POST" name="scoreform" onsubmit="return validateForm(this)">
+    <div class="form-group">
+        <input class="form-control" type="text" value="<?= $_SESSION['first_name'] ?> <?= $_SESSION['last_name'] ?>" name="full_name" readonly>
+    </div>
+
+        <div class="form-group">
+        <input type="text" id="score" class="form-control" name="score" placeholder="Score" required>
+    </div>
+    <div class="form-group">
+        <input type="text" id="xcount" class="form-control" name="xcount" placeholder="X-Count" required>
+    </div>
+    <div class="form-group">
+        <div class="bow">
+            <select name="division" id="division">
+                <option value="compound" id="Compound">Compound</option>
+                <option value="recurve" id="Recurve">Recurve</option>
+                <option value="recurve_bb" id="Recurve_bb">Recurve Barebow</option>
+                <option value="longbow" id="Longbow">Longbow</option>
+                <option value="crossbow" id="Crossbow">Crossbow</option>
+            </select>
+        </div>
+        <input type="hidden" name="anz_num" id="anz_num" value="<?= $_SESSION['anz_num'] ?>">
+        <input type="hidden" name="week" id="week_num" value="<?= $_GET['week'] ?>">
+        <input type="hidden" id="prefered_type" value="<?= $_SESSION['prefered_type'] ?>">
+    </div>
+
+    <input type="submit" class="btn btn-primary" name="submit" value="Submit"></input>
+    <div id="row">
+        <p id="incorrect" style="color: red; padding-left: 36px"></p>
+    </div>
+</form>
+
+<hr>
 
 
 <div class="table-responsive">
