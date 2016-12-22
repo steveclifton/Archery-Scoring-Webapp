63fab5429b73b2c345558d37f29d110862288f1f
Steve <stephen.clifton.1@uni.massey.ac.nz>
2016-12-23 12:01:23 +1300

    Init commit for model

---
+++ Models/AdminConfig.Model.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Archery\Models;
+
+use Archery\Exceptions\CustomException;
+
+use PDO;
+
+
+/**
+ * Class AdminConfig
+ *
+ * Class used to query the database for admin data
+ *
+ * @package
+ */
+class AdminConfig extends Base
+{
+
+
+
+}
