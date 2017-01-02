91019ef250233f04dd473d1c27ceaf3ed05145bd
Steve <stephen.clifton.1@uni.massey.ac.nz>
2017-01-02 15:04:43 +1300

    Add sizing to table cols

--- Views/week.view.php
+++ Views/week.view.php
@@ -2,172 +2,190 @@
 
 <div class="container">
     <legend>Submit Scores</legend>
-<div id="subscores">
-    <?php
-    if (isset($_SESSION['id'])) {
-        foreach ($viewData['archers'] as $archer) { ?>
-            <form class="form-inline" action="/submitscore" method="POST" name="scoreform" onsubmit="return validateForm(this)" style="padding-top: 10px">
-                <div class="form-group">
-                    <input class="form-control" type="text" value="<?= $archer['first_name'] ?> <?= $archer['last_name'] ?>" name="full_name" readonly>
-                </div>
-                <div class="form-group">
-                    <input type="text" id="score" class="form-control" name="score" placeholder="Score" required>
-                </div>
-                <div class="form-group">
-                    <input type="text" id="xcount" class="form-control" name="xcount" placeholder="X-Count" required>
-                </div>
-                <!--Select Bow Division data-->
-                <div class="form-group">
-                    <div class="bow">
-                        <select name="division" id="division">
-                            <option value="compound" id="Compound">Compound</option>
-                            <option value="recurve" id="Recurve">Recurve</option>
-                            <option value="recurve_bb" id="Recurve_bb">Recurve Barebow</option>
-                            <option value="longbow" id="Longbow">Longbow</option>
-                            <option value="crossbow" id="Crossbow">Crossbow</option>
-                        </select>
+    <div id="subscores">
+        <?php
+        if (isset($_SESSION['id'])) {
+            foreach ($viewData['archers'] as $archer) { ?>
+                <form class="form-inline" action="/submitscore" method="POST" name="scoreform"
+                      onsubmit="return validateForm(this)" style="padding-top: 10px">
+                    <div class="form-group">
+                        <input class="form-control" type="text"
+                               value="<?= $archer['first_name'] ?> <?= $archer['last_name'] ?>" name="full_name"
+                               readonly>
                     </div>
-                </div>
-                <!--Hidden data-->
-                <div class="form-group">
-                    <input type="hidden" name="anz_num" id="anz_num" value="<?= $archer['anz_num'] ?>">
-                    <input type="hidden" name="week" id="week_num" value="<?= $_GET['week'] ?>">
-                    <input type="hidden" id="prefered_type" value="<?= $archer['prefered_type'] ?>">
-                </div>
-                <input type="submit" class="btn btn-success" name="submit" value="Submit"></input>
-                <div id="row">
-                    <p id="incorrect" style="color: red; padding-left: 36px"></p>
-                </div>
-            </form>
+                    <div class="form-group">
+                        <input type="text" id="score" class="form-control" name="score" placeholder="Score" required>
+                    </div>
+                    <div class="form-group">
+                        <input type="text" id="xcount" class="form-control" name="xcount" placeholder="X-Count"
+                               required>
+                    </div>
+                    <!--Select Bow Division data-->
+                    <div class="form-group">
+                        <div class="bow">
+                            <select name="division" id="division">
+                                <option value="compound" id="Compound">Compound</option>
+                                <option value="recurve" id="Recurve">Recurve</option>
+                                <option value="recurve_bb" id="Recurve_bb">Recurve Barebow</option>
+                                <option value="longbow" id="Longbow">Longbow</option>
+                                <option value="crossbow" id="Crossbow">Crossbow</option>
+                            </select>
+                        </div>
+                    </div>
+                    <!--Hidden data-->
+                    <div class="form-group">
+                        <input type="hidden" name="anz_num" id="anz_num" value="<?= $archer['anz_num'] ?>">
+                        <input type="hidden" name="week" id="week_num" value="<?= $_GET['week'] ?>">
+                        <input type="hidden" id="prefered_type" value="<?= $archer['prefered_type'] ?>">
+                    </div>
+                    <input type="submit" class="btn btn-success" name="submit" value="Submit"></input>
+                    <div id="row">
+                        <p id="incorrect" style="color: red; padding-left: 36px"></p>
+                    </div>
+                </form>
 
+            <?php } ?>
         <?php } ?>
-    <?php } ?>
-</div>
+    </div>
 
-    <?php if(isset($_SESSION['id'])) { ?>
+    <?php if (isset($_SESSION['id'])) { ?>
         <form class="form-inline" action="/addtempuser" method="POST" name="scoreform" style="padding-top: 10px">
             <input type="button" value="Add Another Archer" id="addarcherbutton" class="btn btn-warning">
             <div class="form-group hidden" id="searcharcherform">
-                <input type="text" name="anz_num" id="searcharcher" class="form-control" placeholder="Search ANZ Number">
+                <input type="text" name="anz_num" id="searcharcher" class="form-control"
+                       placeholder="Search ANZ Number">
                 <input type="submit" id="addsubmit" class="btn btn-success" name="submit" value="Add!"></input>
             </div>
             <input type="hidden" name="week" id="week_num" value="<?= $_GET['week'] ?>">
         </form>
     <?php } ?>
 
-
-
-<div class="table-responsive">
     <div class="container">
-        <h3 style="text-align: center;">Compound</h3>
-        <table class="table table-bordered table-hover">
-            <thead>
+
+        <div class="table-responsive">
+
+            <h3 style="text-align: center;">Compound</h3>
+            <table class="table table-bordered table-hover table-condensed">
+                <thead>
                 <tr>
-                    <th>#</th>
-                    <th>First Name</th>
-                    <th>Last Name</th>
-                    <th>Score</th>
-                    <th>X-Count</th>
+                    <th class="col-md-1">#</th>
+                    <th class="col-md-2">First Name</th>
+                    <th class="col-md-2">Last Name</th>
+                    <th class="col-md-1">Season Average</th>
+                    <th class="col-md-1">Score</th>
+                    <th class="col-md-1">X-Count</th>
                 </tr>
-            </thead>
-            <tbody>
-            <?php
+                </thead>
+                <tbody>
+                <?php
                 $i = 1;
                 foreach ($viewData['scores'] as $data) { ?>
-                    <?php if ($data['division'] == 'compound') {?>
-                    <tr>
-                        <th scope="row"> <?php echo $i; $i++; ?></th>
-                        <td> <?= $data['first_name'] ?></td>
-                        <td> <?=$data['last_name'] ?></td>
-                        <td> <?=$data['score'] ?></td>
-                        <td> <?=$data['xcount'] ?></td>
-                    </tr>
-                        <?php } ?>
+                    <?php if ($data['division'] == 'compound') { ?>
+                        <tr>
+                            <th scope="row"> <?php echo $i;
+                                $i++; ?></th>
+                            <td> <?= $data['first_name'] ?></td>
+                            <td> <?= $data['last_name'] ?></td>
+                            <td>Av</td>
+                            <td> <?= $data['score'] ?></td>
+                            <td> <?= $data['xcount'] ?></td>
+                        </tr>
+                    <?php } ?>
                 <?php } ?>
-            </tbody>
-        </table>
-        <h3 style="text-align: center;">Recurve</h3>
-        <table class="table table-bordered table-hover">
-            <thead>
-            <tr>
-                <th>#</th>
-                <th>First Name</th>
-                <th>Last Name</th>
-                <th>Score</th>
-                <th>X-Count</th>
-            </tr>
-            </thead>
-            <tbody>
-            <?php
-            $i = 1;
-            foreach ($viewData['scores'] as $data) { ?>
-                <?php if ($data['division'] == 'recurve') {?>
-                    <tr>
-                        <th scope="row"> <?php echo $i; $i++; ?></th>
-                        <td> <?= $data['first_name'] ?></td>
-                        <td> <?=$data['last_name'] ?></td>
-                        <td> <?=$data['score'] ?></td>
-                        <td> <?=$data['xcount'] ?></td>
-                    </tr>
+                </tbody>
+            </table>
+
+            <h3 style="text-align: center;">Recurve</h3>
+            <table class="table table-bordered table-hover">
+                <thead>
+                <tr>
+                    <th class="col-md-1">#</th>
+                    <th class="col-md-2">First Name</th>
+                    <th class="col-md-2">Last Name</th>
+                    <th class="col-md-1">Season Average</th>
+                    <th class="col-md-1">Score</th>
+                    <th class="col-md-1">X-Count</th>
+                </tr>
+                </thead>
+                <tbody>
+                <?php
+                $i = 1;
+                foreach ($viewData['scores'] as $data) { ?>
+                    <?php if ($data['division'] == 'recurve') { ?>
+                        <tr>
+                            <th scope="row"> <?php echo $i;
+                                $i++; ?></th>
+                            <td> <?= $data['first_name'] ?></td>
+                            <td> <?= $data['last_name'] ?></td>
+                            <td>Av</td>
+                            <td> <?= $data['score'] ?></td>
+                            <td> <?= $data['xcount'] ?></td>
+                        </tr>
+                    <?php } ?>
                 <?php } ?>
-            <?php } ?>
-            </tbody>
-        </table>
-        </table>
-        <h3 style="text-align: center;">Recurve Barebow</h3>
-        <table class="table table-bordered table-hover">
-            <thead>
-            <tr>
-                <th>#</th>
-                <th>First Name</th>
-                <th>Last Name</th>
-                <th>Score</th>
-                <th>X-Count</th>
-            </tr>
-            </thead>
-            <tbody>
-            <?php
-            $i = 1;
-            foreach ($viewData['scores'] as $data) { ?>
-                <?php if ($data['division'] == 'recurve_bb') {?>
-                    <tr>
-                        <th scope="row"> <?php echo $i; $i++; ?></th>
-                        <td> <?= $data['first_name'] ?></td>
-                        <td> <?=$data['last_name'] ?></td>
-                        <td> <?=$data['score'] ?></td>
-                        <td> <?=$data['xcount'] ?></td>
-                    </tr>
+                </tbody>
+            </table>
+
+            <h3 style="text-align: center;">Recurve Barebow</h3>
+            <table class="table table-bordered table-hover">
+                <thead>
+                <tr>
+                    <th class="col-md-1">#</th>
+                    <th class="col-md-2">First Name</th>
+                    <th class="col-md-2">Last Name</th>
+                    <th class="col-md-1">Season Average</th>
+                    <th class="col-md-1">Score</th>
+                    <th class="col-md-1">X-Count</th>
+                </tr>
+                </thead>
+                <tbody>
+                <?php
+                $i = 1;
+                foreach ($viewData['scores'] as $data) { ?>
+                    <?php if ($data['division'] == 'recurve_bb') { ?>
+                        <tr>
+                            <th scope="row"> <?php echo $i;
+                                $i++; ?></th>
+                            <td> <?= $data['first_name'] ?></td>
+                            <td> <?= $data['last_name'] ?></td>
+                            <td>Av</td>
+                            <td> <?= $data['score'] ?></td>
+                            <td> <?= $data['xcount'] ?></td>
+                        </tr>
+                    <?php } ?>
                 <?php } ?>
-            <?php } ?>
-            </tbody>
-        </table>
-        </table>
-        <h3 style="text-align: center;">Longbow</h3>
-        <table class="table table-bordered table-hover">
-            <thead>
-            <tr>
-                <th>#</th>
-                <th>First Name</th>
-                <th>Last Name</th>
-                <th>Score</th>
-                <th>X-Count</th>
-            </tr>
-            </thead>
-            <tbody>
-            <?php
-            $i = 1;
-            foreach ($viewData['scores'] as $data) { ?>
-                <?php if ($data['division'] == 'longbow') {?>
-                    <tr>
-                        <th scope="row"> <?php echo $i; $i++; ?></th>
-                        <td> <?= $data['first_name'] ?></td>
-                        <td> <?=$data['last_name'] ?></td>
-                        <td> <?=$data['score'] ?></td>
-                        <td> <?=$data['xcount'] ?></td>
-                    </tr>
+                </tbody>
+            </table>
+
+            <h3 style="text-align: center;">Longbow</h3>
+            <table class="table table-bordered table-hover">
+                <thead>
+                <tr>
+                    <th class="col-md-1">#</th>
+                    <th class="col-md-2">First Name</th>
+                    <th class="col-md-2">Last Name</th>
+                    <th class="col-md-1">Season Average</th>
+                    <th class="col-md-1">Score</th>
+                    <th class="col-md-1">X-Count</th>
+                </tr>
+                </thead>
+                <tbody>
+                <?php
+                $i = 1;
+                foreach ($viewData['scores'] as $data) { ?>
+                    <?php if ($data['division'] == 'longbow') { ?>
+                        <tr>
+                            <th scope="row"> <?php echo $i;
+                                $i++; ?></th>
+                            <td> <?= $data['first_name'] ?></td>
+                            <td> <?= $data['last_name'] ?></td>
+                            <td>Av</td>
+                            <td> <?= $data['score'] ?></td>
+                            <td> <?= $data['xcount'] ?></td>
+                        </tr>
+                    <?php } ?>
                 <?php } ?>
-            <?php } ?>
-            </tbody>
-        </table>
+                </tbody>
+            </table>
+        </div>
     </div>
-</div>
