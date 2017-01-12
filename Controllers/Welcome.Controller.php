309c42c5c895ead4afe121f0068c679d698527b8
Steve <stephen.clifton.1@uni.massey.ac.nz>
2017-01-13 11:38:32 +1300

    Removed unused method

--- Controllers/Welcome.Controller.php
+++ Controllers/Welcome.Controller.php
@@ -4,27 +4,8 @@
 namespace Archery\Controllers;
 
 
-use Archery\Models\Score;
-
 class Welcome extends Base
 {
-
-    /**
-     * Gets the users details from SESSION and passes to the view
-     */
-    public function processWelcome()
-    {
-        $this->isNotLoggedIn();
-
-        $viewData['first_name'] = $_SESSION['first_name'];
-        $viewData['last_name'] = $_SESSION['last_name'];
-
-        $scores = new Score();
-        $scores = $scores->liu_getAllScores();
-        $viewData['scores'] = $scores;
-
-        $this->render('Welcome', 'welcome.view', $viewData);
-    }
 
     public function displayRules()
     {
