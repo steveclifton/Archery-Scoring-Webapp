07ea46174b32394efe43006b8400fa92d9d5d351
Steve <stephen.clifton.1@uni.massey.ac.nz>
2017-01-18 12:25:34 +1300

    Add styling

--- Views/base.view.php
+++ Views/base.view.php
@@ -13,28 +13,34 @@
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
         <link rel="stylesheet" href="Views/css/custom.css">
 
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.12.1/css/bootstrap-select.min.css">
+
+        <!-- Latest compiled and minified JavaScript -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.12.1/js/bootstrap-select.min.js"></script>
+
+
+
     </head>
     <body>
         <div class="container-fluid">
             <div class="row">
                 <?php include('Views/layouts/header.php'); ?>
             </div>
+            <div class="row">
+                <h2 style="font-family: 'Droid Sans', sans-serif; text-align: center;">2017 Outdoor League</h2>
+            </div>
         </div>
+        <br>
 
         <div class="page-container">
-            <div class="container-fluid">
-
-                <?php include('Views/layouts/leftside.php'); ?>
-
+            <div class="container">
                 <div class="col-sm-6 col-md-10">
-                    <?= "<h2 style=\"font-family: 'Droid Sans', sans-serif; text-align: center; \">2017 Outdoor League Series</h2>" ?>
                     <?php include($viewName . '.php'); ?>
                 </div>
             </div>
         </div>
 
-<!--        <div class="container">-->
-<!--            --><?php //include('../Views/layouts/footer.php')?>
-<!--        </div>-->
+
     </body>
 </html>
