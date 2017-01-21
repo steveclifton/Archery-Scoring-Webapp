bb1156cec505c214668e1f12842da1c94fa27313
Steve <stephen.clifton.1@uni.massey.ac.nz>
2017-01-21 18:43:58 +1300

    Remove unused route

--- index.php
+++ index.php
@@ -47,15 +47,12 @@ else if ($uri == 'register') {
     $authentication->register();
 }
 
-else if ($uri == 'registerprofile') {
-    $authentication = new Authentication();
-    $authentication->registerProfile();
-}
-
 else if ($uri == 'updatesetup') {
     $admin = new Event();
     $admin->setSetup();
 }
+
+
 
 else if ($uri == 'admin') {
     $admin = new Admin();
