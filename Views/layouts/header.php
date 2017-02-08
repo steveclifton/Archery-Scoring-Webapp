24081ed7726387f9efce4a5d6afa114a8c08da54
Steve <stephen.clifton.1@uni.massey.ac.nz>
2017-02-08 21:05:12 +1300

    Update method to use aJax

--- Views/layouts/header.php
+++ Views/layouts/header.php
@@ -15,46 +15,35 @@
                 <li><a href="mailto:anzindoor@gmail.com">Contact</a></li>
                 <li><a href="/register">Register</a></li>
                 <li><a href="http://www.facebook.com/ArcheryNZIndoor" target="_blank">Facebook</a></li>
-                <?php
-                    if(isset($_SESSION['user_type']) && $_SESSION['user_type'] == 'admin') {
-                        echo "<li><a href=\"admin\">Admin</a></li>";
-                    }
-                ?>
+                <?php if(isset($_SESSION['user_type']) && $_SESSION['user_type'] == 'admin') { ?>
+                    <li><a href="/admin">Admin</a></li>
+                <?php } ?>
             </ul>
-            <div class="col-xs-2 pull-left">
-                <p id="noStudent" style="color: red; text-align: end; padding-top: 14px">
-                    <?php
-                        if(isset($_SESSION['failedLogin'])) {
-                            echo $_SESSION['failedLogin'];
-                            unset($_SESSION['failedLogin']);
-                        }
-                    ?>
-                </p>
-            </div>
+
             <ul class="nav navbar-nav navbar-right">
                 <?php
                     if (isset($_SESSION['id'])) {
                         $username = $_SESSION['first_name'] . " " . $_SESSION['last_name'];
                         echo "
-                                <li class='dropdown'>
-                                    <a href=\"#\" class=\"dropdown-toggle\" data-toggle=\"dropdown\">$username <b class=\"caret\"></b></a>
-                                    <ul class=\"dropdown-menu\">
-                                        <li><a href='/userprofile'>Profile</a></li>
-                                        <li><a href='/logout'>Logout</a></li>
-                                    </ul>
-                                </li>
+                            <li class='dropdown'>
+                                <a href=\"#\" class=\"dropdown-toggle\" data-toggle=\"dropdown\">$username <b class=\"caret\"></b></a>
+                                <ul class=\"dropdown-menu\">
+                                    <li><a href='/userprofile'>Profile</a></li>
+                                    <li><a href='/logout'>Logout</a></li>
+                                </ul>
+                            </li>
                         ";
                     } else {
                         echo "
-                                <form class=\"navbar-form navbar-right navbar-fixed\" method='post' action=\"login\">
+                            <form class=\"navbar-form navbar-right navbar-fixed\" id=\"loginForm\" method='post' action=\"#\" onsubmit=\"return checkLogin()\">
                                 <div class=\"form-group\">
-                                <input type=\"email\" class=\"form-control\" name=\"email\" placeholder=\"Email\">
+                                    <input type=\"email\" class=\"form-control\" name=\"email\" placeholder=\"Email\">
                                 </div>
                                 <div class=\"form-group\">
-                                <input type=\"password\" class=\"form-control\" name=\"password\" placeholder=\"Password\">
+                                    <input type=\"password\" class=\"form-control\" name=\"password\" placeholder=\"Password\">
                                 </div>
                                 <button type=\"submit\" class=\"btn btn-default\">Sign In</button>
-                                </form>
+                            </form>
                         ";
                     }
                 ?>
