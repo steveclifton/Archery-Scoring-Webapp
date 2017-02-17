c8a7f2675e5b1f492a3e3127e4a81885025ab369
Steve <stephen.clifton.1@uni.massey.ac.nz>
2017-02-17 23:25:32 +1300

    Initial commit

---
+++ Models/Divisions.Model.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace Archery\Models;
+
+use Archery\Configurations\Event;
+use PDO;
+
+class Divisions extends Base
+{
+    public function getDivision()
+    {
+
+    }
+
+    public function setDivision()
+    {
+
+    }
+}
