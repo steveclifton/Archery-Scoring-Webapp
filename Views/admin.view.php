2a6f034c84a435496faa95dadc34456b1c1bd721
Steve <stephen.clifton.1@uni.massey.ac.nz>
2017-02-23 22:10:07 +1300

    Add id

--- Views/admin.view.php
+++ Views/admin.view.php
@@ -10,11 +10,11 @@
         <table class="table table-bordered table-hover">
             <thead>
             <tr>
+                <th>Join_Users ID</th>
                 <th>First Name</th>
                 <th>Last Name</th>
                 <th>Anz Num</th>
                 <th>Email</th>
-                <th>Club</th>
                 <th>Authorise</th>
             </tr>
             </thead>
@@ -23,11 +23,11 @@
                 foreach ($viewData['pending_users'] as $user) { ?>
                     <tr>
                         <form action="/confirmusers" method="POST" name="usersconfirm">
+                            <td><input type="text" value="<?= $user['id'] ?>" name="id"></td>
                             <td><input type="text" value="<?= $user['first_name'] ?>" name="first_name"></td>
                             <td><input type="text" value="<?= $user['last_name'] ?>" name="last_name"></td>
                             <td><input type="text" value="<?= $user['anz_num'] ?>" name="anz_num"></td>
                             <td><input type="text" value="<?= $user['email'] ?>" name="email"></td>
-                            <td><input type="text" value="<?= $user['club'] ?>" name="club"></td>
                             <td><input id="submit" type="submit" name="submit" class="btn btn-success" value="Authorise">
                             </td>
                         </form>
