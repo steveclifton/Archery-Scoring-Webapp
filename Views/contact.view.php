8096d129060c39c50c1375b912c79cb2ce3e0f35
Steve <stephen.clifton.1@uni.massey.ac.nz>
2017-03-04 12:56:04 +1300

    Add contact to admin

---
+++ Views/contact.view.php
@@ -0,0 +1,39 @@
+<div class="container">
+    <div class="row">
+        <div class="col-xs-6 col-xs-offset-3 col-sm-6 col-sm-offset-5 col-md-6 col-md-offset-3">
+            <form class="form-horizontal" id="formContact" method="post" action="#" onsubmit="return false" >
+                <fieldset style="text-align: center">
+                    <div id="legend">
+                        <legend>Contact</legend>
+                        <p>Send Message to ArcheryOSA</p>
+                    </div>
+                    <div class="control-group">
+                        <label class="control-label">Name</label>
+                        <div class="controls">
+                            <input type="text" id="name" name="last_name" placeholder="" class="input-xlarge" required>
+                        </div>
+                    </div>
+                    <div class="control-group">
+                        <label class="control-label">Email</label>
+                        <div class="controls">
+                            <input type="email" id="email" name="email" placeholder="" class="input-xlarge" required>
+                        </div>
+                    </div>
+                    <div class="control-group">
+                        <label class="control-label">Message</label>
+                        <div class="controls">
+                            <textarea type="text" id="message" name="message" rows="10" cols="50"></textarea>
+                        </div>
+                    </div>
+                    <br><br>
+                    <div class="control-group">
+                        <div class="controls">
+                            <button class="btn btn-success" id="registerAccount">Submit</button>
+                        </div>
+                    </div>
+                </fieldset>
+            </form>
+        </div>
+    </div>
+</div>
+<br><br><br><br>
