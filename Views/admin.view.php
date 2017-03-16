3426b440ac0791c86ca94c6f49ec1eb7f1cfa98c
Steve <stephen.clifton.1@uni.massey.ac.nz>
2017-03-16 20:01:34 +1300

    Removed unused fields

--- Views/admin.view.php
+++ Views/admin.view.php
@@ -9,9 +9,7 @@
             <table class="table table-bordered table-hover">
                 <thead>
                     <tr>
-                        <th class="col-md-1">Join_Users ID</th>
                         <th class="col-md-1">User_ID</th>
-                        <th class="col-md-1">Associate_ID</th>
                         <th class="col-md-1">First Name</th>
                         <th class="col-md-1">Last Name</th>
                         <th class="col-md-1">Anz Num</th>
@@ -25,8 +23,6 @@
                     <tr>
                         <form action="/confirmusers" method="POST" name="usersconfirm">
                             <td><input type="text" value="<?= $user['id'] ?>" name="id"></td>
-                            <td><input type="text" value="<?= $user['user_id'] ?>" name="user_id"></td>
-                            <td><input type="text" value="<?= $user['associate_id'] ?>" name="associate_id"></td>
                             <td><input type="text" value="<?= $user['first_name'] ?>" name="first_name"></td>
                             <td><input type="text" value="<?= $user['last_name'] ?>" name="last_name"></td>
                             <td><input type="text" value="<?= $user['anz_num'] ?>" name="anz_num"></td>
@@ -61,41 +57,6 @@
                     <div class="controls">
                         <input type="text" id="numweeks" name="numweeks" placeholder="" class="input-xlarge"
                                value="<?= $viewData['num_weeks'] ?>" required>
-                    </div>
-                </div>
-                <div class="control-group">
-                    <label class="control-label">Current Event</label>
-                    <div class="controls">
-                        <input type="text" id="currentevent" name="currentevent" placeholder="" class="input-xlarge"
-                               value="<?= $viewData['current_event'] ?>" required>
-                    </div>
-                </div>
-                <div class="control-group">
-                    <label class="control-label">Current Round Distance</label>
-                    <div class="controls">
-                        <input type="text" id="currentround" name="currentround" placeholder="" class="input-xlarge"
-                               value="<?= $viewData['current_round'] ?>" required>
-                    </div>
-                </div>
-                <div class="control-group">
-                    <label class="control-label">Max Score</label>
-                    <div class="controls">
-                        <input type="text" id="maxscore" name="maxscore" placeholder="" class="input-xlarge"
-                               value="<?= $viewData['max_score'] ?>" required>
-                    </div>
-                </div>
-                <div class="control-group">
-                    <label class="control-label">Max X-Count</label>
-                    <div class="controls">
-                        <input type="text" id="maxxcount" name="maxxcount" placeholder="" class="input-xlarge"
-                               value="<?= $viewData['max_xcount'] ?>" required>
-                    </div>
-                </div>
-                <div class="control-group">
-                    <label class="control-label">Database Table Name</label>
-                    <div class="controls">
-                        <input type="text" id="tablename" name="tablename" placeholder="" class="input-xlarge"
-                               value="<?= $viewData['db_name'] ?>" required>
                     </div>
                 </div>
                 <br>
