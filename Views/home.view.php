13d517d315102aaccc0e8e907062a8973a526bb5
Steve <stephen.clifton.1@uni.massey.ac.nz>
2017-03-16 22:19:22 +1300

    Initial commit

--- Views/home.view.php
+++ Views/home.view.php
@@ -1,1 +1,14 @@
-<h3 style="text-align: center">Welcome, homepage being worked on</h3>
+<div class="container">
+    <div class="row">
+        <h2 style="text-align: center">Archery OSA</h2>
+    </div>
+
+    <div class="row">
+        <p style="text-align: center">
+            Welcome to the Archery OSA
+        </p>
+    </div>
+
+
+</div>
+
